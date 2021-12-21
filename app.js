af1942a32de4867ab791c9002b224f17040070c0
LeyCod <emmanuel_lg34@hotmail.com>
2021-12-21 01:27:51 +0100

    feat: README upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+button.addEventListener('click', function (){
+const newColor = newRandColor();
+document.body.style.backgroundColor = newColor;
+h1.innerText = newColor;
+});
+
+const newRandColor = () => {
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+};
